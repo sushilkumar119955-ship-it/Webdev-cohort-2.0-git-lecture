745ed7b6b6ac5bc2debd8c08834e70cb0e0eb810
sushilkumar119955-ship-it <sushilkumar119955@gmail.com>
2026-09-07 05:27:41 +0530

    feature/multiply (#1)
    * add another multiply function call to index.js for enhanced operation
    
    * fix: correct multiply function definition and improve formatting

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ function sub{
     return a-b
 
 }
-
+function multiply(a,b){
+    return a*b
+}
     add(2,5)
     add(10,23)
     add(40,42)
 
 
     sub(10,3)
+    multiply(2,3)
+    multiply(20,110)
+
 
 
