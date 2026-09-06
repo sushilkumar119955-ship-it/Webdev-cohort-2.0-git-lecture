78471796a871ee65f79bc7d19db16354a47832fc
sushil-kumar <sushilkumar119955@gmail.com>
2026-09-07 05:23:57 +0530

    fix: correct multiply function definition and improve formatting

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function sub{
     return a-b
 
 }
-
+function multiply(a,b){
+    return a*b
+}
     add(2,5)
     add(10,23)
     add(40,42)
@@ -17,6 +19,6 @@ function sub{
     sub(10,3)
     multiply(2,3)
     multiply(20,110)
-    
+
 
 
