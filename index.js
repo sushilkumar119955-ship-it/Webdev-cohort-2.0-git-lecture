f2abe5ca01a8353361b1a5a817eb6d7612ad3d9f
SUSHIL SiNGH SAROHA <sushilkumar119955@gmail.com>
2026-09-06 16:21:24 +0530

    i have just merged changes from feature/a branch

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 function add(a,b){
     return a+b
+
+}
+
+function sub{
+    if(!(a>b))throw new Error('num a must be greater than b')
+    return a-b
+
+}
+
     add(2,5)
     add(10,23)
     add(40,42)
-    
-}
+
+
+    sub(10,3)
+
+
